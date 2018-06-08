3cb374636981ad926e201e3cdcbef6b01b835631
Adam Lesperance <adam.lesperance@target.com>
2018-06-07 23:18:47 -0500

    Get the yara scanner working (doesn't interact with db yet though

--- models.py
+++ models.py
@@ -1,15 +1,27 @@
 """Yara AUCR plugin default database tables."""
+import os
+
 # coding=utf-8
 import udatetime as datetime
-from app import db
-from yaml_info.yamlinfo import YamlInfo
 import yara
-import os
 from sqlalchemy import event
+
+from app import db
+
+
+def check_dir(dir, name):
+    if not os.path.exists(dir):
+        raise RuntimeError("The {} dir '{}' must exist but it doesn't!".format(name, dir))
 
 
 class YaraRules(db.Model):
     """Yara data default table for aucr."""
+
+    __mal_dir = '/upload/malware'
+    __fp_dir = '/upload/fp'
+
+    check_dir(__mal_dir, 'malware')
+    check_dir(__fp_dir, 'fp')
 
     __tablename__ = 'yara_rules'
     id = db.Column(db.Integer, primary_key=True)
@@ -23,23 +35,35 @@ class YaraRules(db.Model):
     def __repr__(self):
         return '<Yara {}>'.format(self.yara_name)
 
-    def test_yara(self, provided_key):
-        results = []
+    @staticmethod
+    def __scan(scanner, d, findings):
+        for file in os.scandir(d):
+            if file.is_file():
+                try:
+                    if scanner.match(file.path):
+                        findings.append(str(file.name))
+                except Exception as e:
+                    print("Yara issue; " + str(e))
+
+    def test_yara(self):
+        good_matches = []
+        bad_matches = []
+
         try:
-            rules = yara.compile(source=provided_key)
-            malz_path = os.listdir('CTFd/plugins/CTFd-yarachallenge/assets/malware')
-            for file_names in malz_path:
-                test_file = ('CTFd/plugins/CTFd-yarachallenge/assets/malware/' + file_names)
-                matches = rules.match(test_file)
-                if matches:
-                    match_name = str(file_names)
-                    results.append(match_name)
+            scanner = yara.compile(source=self.yara_rules)
+            YaraRules.__scan(scanner, YaraRules.__mal_dir, good_matches)
+            YaraRules.__scan(scanner, YaraRules.__fp_dir, bad_matches)
+            return good_matches, bad_matches
         except Exception as e:
             # logging.warning("Not a valid Answer" + str(e))
-            return results
+            print("Something broke " + str(e))
+            return [], []
 
 
 @event.listens_for(db.session, 'before_flush')
 def receive_before_flush(session, flush_context, instances):
     for t in (x for x in session.new.union(session.dirty) if isinstance(x, YaraRules)):
-        t.test_yara()
+        good, bad = t.test_yara()
+        print('GOOD Yara: ' + ' -- '.join(good))
+        print('BAD Yara:  ' + ' -- '.join(bad))
+        ()
