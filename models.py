6e07653f624ee16d3e13bd8f23da6484133d7646
Adam Lesperance <adam.lesperance@target.com>
2018-06-07 22:00:02 -0500

    Capture when yara is inserted/updated

--- models.py
+++ models.py
@@ -3,6 +3,9 @@
 import udatetime as datetime
 from app import db
 from yaml_info.yamlinfo import YamlInfo
+import yara
+import os
+from sqlalchemy import event
 
 
 class YaraRules(db.Model):
@@ -19,3 +22,24 @@ class YaraRules(db.Model):
 
     def __repr__(self):
         return '<Yara {}>'.format(self.yara_name)
+
+    def test_yara(self, provided_key):
+        results = []
+        try:
+            rules = yara.compile(source=provided_key)
+            malz_path = os.listdir('CTFd/plugins/CTFd-yarachallenge/assets/malware')
+            for file_names in malz_path:
+                test_file = ('CTFd/plugins/CTFd-yarachallenge/assets/malware/' + file_names)
+                matches = rules.match(test_file)
+                if matches:
+                    match_name = str(file_names)
+                    results.append(match_name)
+        except Exception as e:
+            # logging.warning("Not a valid Answer" + str(e))
+            return results
+
+
+@event.listens_for(db.session, 'before_flush')
+def receive_before_flush(session, flush_context, instances):
+    for t in (x for x in session.new.union(session.dirty) if isinstance(x, YaraRules)):
+        t.test_yara()
